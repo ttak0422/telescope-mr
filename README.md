a8fe7c9a3459fe9ceb1af63264b17e035445d30f
ttak0422 <ttak0422@gmail.com>
2025-08-14 23:00:39 +0900

    Initial commit: Add telescope extension for vim-mr integration
    - Add LICENSE file with MIT license
    - Add README with basic usage instructions for mru, mrr, and mrw commands
    - Implement telescope extension with mr.vim integration for recent file navigation

---
+++ README.md
@@ -0,0 +1,13 @@
+# telescope-mr
+
+## Requirements
+- [vim-mr](https://github.com/lambdalisue/vim-mr)
+
+## Usage
+
+```lua
+:Telescope mru
+:Telescope mrw
+:Telescope mrr
+```
+
---
+++ lua/telescope/_extensions/mr.lua
@@ -0,0 +1,31 @@
+local telescope = require("telescope")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local conf = require("telescope.config").values
+
+-- Common picker function for MR (mr.vim) integration
+local function create_mr_picker(opts, title, list_function)
+	pickers
+		.new(opts, {
+			prompt_title = title,
+			finder = finders.new_table({
+				results = list_function(),
+			}),
+			sorter = conf.generic_sorter(opts),
+		})
+		:find()
+end
+
+return telescope.register_extension({
+	exports = {
+		mru = function(opts)
+			create_mr_picker(opts, "MRU", vim.fn["mr#mru#list"])
+		end,
+		mrr = function(opts)
+			create_mr_picker(opts, "MRR", vim.fn["mr#mrr#list"])
+		end,
+		mrw = function(opts)
+			create_mr_picker(opts, "MRW", vim.fn["mr#mrw#list"])
+		end,
+	},
+})
